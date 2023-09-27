b9d81dfd135d97f8e3c94f751c37a87e1bfd0b93
ThomasNeuberger <23504477+thomasneuberger@users.noreply.github.com>
2023-09-27 11:24:48 +0200

    Add page for displaying user information

---
+++ Horscht.App/Pages/UserInfo.razor.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Authorization;
+
+namespace Horscht.App.Pages;
+public partial class UserInfo
+{
+    [Inject]
+    private AuthenticationStateProvider? AuthenticationStateProvider { get; set; }
+
+    private Claim[] _claims = Array.Empty<Claim>();
+
+    protected override async Task OnInitializedAsync()
+    {
+        if (AuthenticationStateProvider is not null)
+        {
+            var state = await AuthenticationStateProvider.GetAuthenticationStateAsync();
+
+            _claims = state.User.Claims.ToArray();
+        }
+    }
+}
