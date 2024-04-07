74a825e42944bb0d617b2ecd85039ae28a115fbc
Thomas Neuberger <23504477+thomasneuberger@users.noreply.github.com>
2024-04-07 12:39:30 +0200

    Add health check to importer app (#6)

---
+++ Horscht.Importer/Controllers/HealthController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Horscht.Importer.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+public class HealthController : ControllerBase
+{
+    [HttpGet]
+    [AllowAnonymous]
+    public IActionResult Get()
+    {
+        return Ok();
+    }
+}
